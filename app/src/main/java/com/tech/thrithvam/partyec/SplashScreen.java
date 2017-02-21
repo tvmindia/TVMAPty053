692292835e1ff2a71d2f47d778e6876a8a3f88ab
tvm-sreejith <sreejith.cv@thrithvam.ae>
2017-02-21 17:36:33 +0530

    splash screen

--- app/src/main/java/com/tech/thrithvam/partyec/SplashScreen.java
+++ app/src/main/java/com/tech/thrithvam/partyec/SplashScreen.java
@@ -4,6 +4,13 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import io.codetail.animation.SupportAnimator;
+import io.codetail.animation.ViewAnimationUtils;
+import io.codetail.widget.RevealFrameLayout;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -11,6 +18,35 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        final ImageView myView = (ImageView) findViewById(R.id.test);
+
+
+
+        // Android native animator
+        myView.post(new Runnable(){
+
+            @Override
+            public void run()
+            {
+
+
+                // get the center for the clipping circle
+                int cx = (myView.getLeft() + myView.getRight()) / 2;
+                int cy = (myView.getTop() + myView.getBottom()) / 2;
+
+                // get the final radius for the clipping circle
+                int dx = Math.max(cx, myView.getWidth() - cx);
+                int dy = Math.max(cy, myView.getHeight() - cy);
+                float finalRadius = (float) Math.hypot(dx, dy);
+        SupportAnimator animator =
+                ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0, finalRadius);
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.setDuration(2500);
+        animator.start();
+
+            }
+        });
 
 
         Handler handler = new Handler();
