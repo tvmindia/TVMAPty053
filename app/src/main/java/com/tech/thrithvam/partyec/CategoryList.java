029d4beab8fa85ad7aac6932393df7979fcc6302
tvm-sreejith <sreejith.cv@thrithvam.ae>
2017-03-01 14:19:16 +0530

    searchview in actionbar

--- app/src/main/java/com/tech/thrithvam/partyec/CategoryList.java
+++ app/src/main/java/com/tech/thrithvam/partyec/CategoryList.java
@@ -1,10 +1,6 @@
 package com.tech.thrithvam.partyec;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.widget.SearchView;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 public class CategoryList extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -49,7 +44,7 @@ public class CategoryList extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.category_list, menu);
+        getMenuInflater().inflate(R.menu.menu_actionbar, menu);
         common.SearchViewActionBarInitialisation(this,menu);
         return true;
     }
