78da56c58d3051a0dd6ad52a2af7c349cb32e75a
tvm-sreejith <sreejith.cv@thrithvam.ae>
2017-03-02 12:36:27 +0530

    custom adapter + nothing to display screen + loading from webservice

--- app/src/main/java/com/tech/thrithvam/partyec/CategoryList.java
+++ app/src/main/java/com/tech/thrithvam/partyec/CategoryList.java
@@ -1,5 +1,7 @@
 package com.tech.thrithvam.partyec;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -9,18 +11,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.wang.avi.AVLoadingIndicatorView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CategoryList extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     Common common=new Common();
+    ListView categoryListView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
+        categoryListView=(ListView)findViewById(R.id.category_listview);
+        new GetCategoyList().execute();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -30,6 +50,109 @@ public class CategoryList extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+    public class GetCategoyList extends AsyncTask<Void , Void, Void> {
+        int status;StringBuilder sb;
+        String strJson, postData;
+        JSONArray jsonArray;
+        String msg;
+        boolean pass=false;
+        AVLoadingIndicatorView loadingIndicator =(AVLoadingIndicatorView)findViewById(R.id.loading_indicator);
+        ArrayList<String[]> dataArrayList=new ArrayList<>();
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            loadingIndicator.setVisibility(View.VISIBLE);
+            //----------encrypting ---------------------------
+            // usernameString=cryptography.Encrypt(usernameString);
+        }
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            String url =getResources().getString(R.string.url) + "Webservices/document.asmx/TestJSON";
+            HttpURLConnection c = null;
+            try {
+                postData =  "{\"test\":\"" + "testdatainput"+ "\"}";
+                URL u = new URL(url);
+                c = (HttpURLConnection) u.openConnection();
+                c.setRequestMethod("POST");
+                c.setRequestProperty("Content-type", "application/json; charset=utf-16");
+                c.setRequestProperty("Content-length", Integer.toString(postData.length()));
+                c.setDoInput(true);
+                c.setDoOutput(true);
+                c.setUseCaches(false);
+                c.setConnectTimeout(10000);
+                c.setReadTimeout(10000);
+                DataOutputStream wr = new DataOutputStream(c.getOutputStream());
+                wr.writeBytes(postData);
+                wr.flush();
+                wr.close();
+                status = c.getResponseCode();
+                switch (status) {
+                    case 200:
+                    case 201: BufferedReader br = new BufferedReader(new InputStreamReader(c.getInputStream()));
+                        sb = new StringBuilder();
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            sb.append(line).append("\n");
+                        }
+                        br.close();
+                        int a=sb.indexOf("[");
+                        int b=sb.lastIndexOf("]");
+                        strJson=sb.substring(a, b + 1);
+                        //   strJson=cryptography.Decrypt(strJson);
+                        strJson="{\"JSON\":" + strJson.replace("\\\"","\"").replace("\\\\","\\") + "}";
+                }
+            } catch (Exception ex) {
+                Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+                msg=ex.getMessage();
+            } finally {
+                if (c != null) {
+                    try {
+                        c.disconnect();
+                    } catch (Exception ex) {
+                        Logger.getLogger(getClass().getName()).log(Level.SEVERE, null, ex);
+                        msg=ex.getMessage();
+                    }
+                }
+            }
+            if(strJson!=null)
+            {try {
+                JSONObject jsonRootObject = new JSONObject(strJson);
+                jsonArray = jsonRootObject.optJSONArray("JSON");
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    msg=jsonObject.optString("Message");
+                    pass=jsonObject.optBoolean("Flag",true);
+                    String[] data=new String[5];
+                    data[0]=jsonObject.optString("Name");
+                    data[1]=jsonObject.optString("AlbumName");
+                    dataArrayList.add(data);
+                }
+            } catch (Exception ex) {
+                msg=ex.getMessage();
+            }}
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            loadingIndicator.setVisibility(View.GONE);
+            Toast.makeText(CategoryList.this, strJson, Toast.LENGTH_SHORT).show();
+            if(!pass) {
+                Intent noItemsIntent=new Intent(CategoryList.this,NothingToDisplay.class);
+                noItemsIntent.putExtra("msg",msg);
+                noItemsIntent.putExtra("activityHead","Shopping");
+                startActivity(noItemsIntent);
+                finish();
+            }
+            else {
+                CustomAdapter adapter=new CustomAdapter(CategoryList.this, dataArrayList,"CategoryList");
+                categoryListView.setAdapter(adapter);
+                categoryListView.setVisibility(View.VISIBLE);
+
+            }
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/tech/thrithvam/partyec/NothingToDisplay.java
@@ -0,0 +1,38 @@
+package com.tech.thrithvam.partyec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+public class NothingToDisplay extends AppCompatActivity {
+    Bundle extras;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nothing_to_display);
+        extras=getIntent().getExtras();
+        //Fonts---------------
+
+        final TextView nothingLabel=(TextView)findViewById(R.id.nothing_label);
+        if(getIntent().hasExtra("activityHead")){
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            toolbar.setTitle(extras.getString("activityHead"));
+            setSupportActionBar(toolbar);
+        }
+        if(getIntent().hasExtra("msg")){
+            nothingLabel.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    nothingLabel.setText(extras.getString("msg"));
+                    return true;
+                }
+            });
+        }
+    }
+    public void back(View view)
+    {
+        onBackPressed();
+    }
+}
