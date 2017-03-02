5a6cc26ef5c993d78728ab0ab29096a6a272810c
tvm-sreejith <sreejith.cv@thrithvam.ae>
2017-03-02 17:27:01 +0530

    glide image loading

--- app/src/main/java/com/tech/thrithvam/partyec/CustomAdapter.java
+++ app/src/main/java/com/tech/thrithvam/partyec/CustomAdapter.java
@@ -1,18 +1,12 @@
 package com.tech.thrithvam.partyec;
 
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -26,8 +20,8 @@ public class CustomAdapter extends BaseAdapter{
     private String calledFrom;
     private SimpleDateFormat formatted;
     private Calendar cal;
-    Common common;
-    public CustomAdapter(Context context, ArrayList<String[]> objects, String calledFrom) {
+    private Common common;
+    CustomAdapter(Context context, ArrayList<String[]> objects, String calledFrom) {
         // super(context, textViewResourceId, objects);
         adapterContext=context;
         inflater = ( LayoutInflater )context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
