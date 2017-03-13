458f5928b1c17786d11e76f63d9d512ae241d1e2
tvm-sreejith <sreejith.cv@thrithvam.ae>
2017-03-13 17:03:45 +0530

    Contact Us screen input and validation

--- app/src/main/java/com/tech/thrithvam/partyec/ContactUs.java
+++ app/src/main/java/com/tech/thrithvam/partyec/ContactUs.java
@@ -12,16 +12,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.wang.avi.AVLoadingIndicatorView;
 
 public class ContactUs extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
     Common common=new Common();
+    EditText name,email,phone,comment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        name=(EditText)findViewById(R.id.name);
+        email=(EditText)findViewById(R.id.email);
+        phone=(EditText)findViewById(R.id.phone);
+        comment=(EditText)findViewById(R.id.comment);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -33,7 +43,25 @@ public class ContactUs extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
         common.NavigationBarHeaderClick(this,navigationView);
     }
-
+    public void submitRequest(View view){
+        if(name.getText().toString().length()==0 || !name.getText().toString().matches(common.UserNameRegularExpression)){
+            name.setError(getResources().getString(R.string.give_valid));
+            name.requestFocus();
+        }
+        else if( !android.util.Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()) {
+            email.setError(getResources().getString(R.string.give_valid));
+            email.requestFocus();
+        }
+        else if(comment.getText().toString().length()==0){
+            comment.setError(getResources().getString(R.string.give_valid));
+            comment.requestFocus();
+        }
+        else{
+            AVLoadingIndicatorView loadingIndicatorView=(AVLoadingIndicatorView)findViewById(R.id.loading_indicator);
+            loadingIndicatorView.setVisibility(View.VISIBLE);
+            view.setVisibility(View.GONE);
+        }
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
