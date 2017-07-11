e60c5fdd31bbb37d3be16a0235cde4e052b9f913
tvm-sreejith <sreejith.cv@thrithvam.ae>
2017-07-11 10:01:25 +0530

    Global search

--- app/src/main/java/com/tech/thrithvam/partyec/Search.java
+++ app/src/main/java/com/tech/thrithvam/partyec/Search.java
@@ -1,6 +1,7 @@
 package com.tech.thrithvam.partyec;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,12 +15,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.TextView;
+
+import com.wang.avi.AVLoadingIndicatorView;
+
+import java.util.ArrayList;
 
 public class Search extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     SearchView searchView;
+    ArrayList<AsyncTask> asyncTasks=new ArrayList<>();
+    TextView enterToSearch;
+    TextView noItems;
+    GridView allProductsGrid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,10 +45,62 @@ public class Search extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
+        enterToSearch=(TextView)findViewById(R.id.enter_to_search);
+        noItems=(TextView)findViewById(R.id.no_items);
+        allProductsGrid=(GridView) findViewById(R.id.all_products_grid);
+        loadProducts(getIntent().getExtras().getString("searchkey"));
+
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         Common.NavigationBarHeaderClick(this,navigationView);
     }
+    CustomAdapter adapterAllProducts;
+    public void loadProducts(String searchKeys){
+        noItems.setVisibility(View.GONE);
+        enterToSearch.setVisibility(View.GONE);
+        searchKeys=searchKeys.trim();
+        if(searchKeys.length()==0)  return;
+        searchKeys=searchKeys.replace(" ",",");
+        //Threading--------------------------------------------------
+        final Common common=new Common();
+        String webService="api/product/ProductsGloablSearching";
+        String postData =  "{\"filterCriteriaCSV\":\""+searchKeys+"\"}";
+        AVLoadingIndicatorView loadingIndicator=(AVLoadingIndicatorView) findViewById(R.id.loading_indicator);
+        allProductsGrid.setVisibility(View.GONE);
+        String[] dataColumns={"Name","ImageURL","ID","StickerURL","TotalPrice","DiscountAmount","StockAvailable","SupplierName"};
+        Runnable postThread=new Runnable() {
+            @Override
+            public void run() {
+                allProductsGrid.setVisibility(View.VISIBLE);
+                adapterAllProducts=new CustomAdapter(Search.this, common.dataArrayList,"AllProducts",0);
+                allProductsGrid.setAdapter(adapterAllProducts);
+                allProductsGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Intent intent=new Intent(Search.this,ProductDetails.class);
+                        intent.putExtra("productID",common.dataArrayList.get(position)[2]);
+                        startActivity(intent);
+                    }
+                });
+            }
+        };
+        Runnable postFailThread=new Runnable() {
+            @Override
+            public void run() {
+                noItems.setVisibility(View.VISIBLE);
+                enterToSearch.setVisibility(View.GONE);
+            }
+        };
+        common.AsynchronousThread(Search.this,
+                webService,
+                postData,
+                loadingIndicator,
+                dataColumns,
+                postThread,
+                postFailThread);
+        asyncTasks.add(common.asyncTask);
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -45,6 +108,9 @@ public class Search extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
+            for(int i=0;i<asyncTasks.size();i++){
+                asyncTasks.get(i).cancel(true);
+            }
             super.onBackPressed();
         }
     }
@@ -56,26 +122,41 @@ public class Search extends AppCompatActivity
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                return false;
+                loadProducts(searchView.getQuery().toString().trim());
+                return true;
             }
             @Override
             public boolean onQueryTextChange(String newText) {
-              /*  if(adapter!=null){
-                    adapter.getFilter(1).filter(searchView.getQuery().toString().trim());
-                    TextView noItems=(TextView)findViewById(R.id.no_items);
-                    noItems.setVisibility(categoryListView.getChildCount()>0?View.INVISIBLE:View.VISIBLE);
-                }*/
+                if(adapterAllProducts!=null){
+                    adapterAllProducts.getFilter(0).filter(searchView.getQuery().toString().trim());
+                    if(allProductsGrid.getChildCount()>0){
+                        enterToSearch.setVisibility(View.INVISIBLE);
+                        noItems.setVisibility(View.GONE);
+                    }
+                    else{
+                        enterToSearch.setVisibility(View.VISIBLE);
+                        noItems.setVisibility(View.GONE);
+                    }
+                }
                 return true;
             }
         });
         searchView.setOnCloseListener(new SearchView.OnCloseListener() {
             @Override
             public boolean onClose() {
-               /* TextView noItems=(TextView)findViewById(R.id.no_items);
-                noItems.setVisibility(categoryListView.getChildCount()>0?View.INVISIBLE:View.VISIBLE);*/
+                if(allProductsGrid.getChildCount()>0){
+                    enterToSearch.setVisibility(View.INVISIBLE);
+                    noItems.setVisibility(View.GONE);
+                }
+                else{
+                    enterToSearch.setVisibility(View.VISIBLE);
+                    noItems.setVisibility(View.GONE);
+                }
                 return false;
             }
         });
+        searchView.setQuery(getIntent().getExtras().getString("searchkey"),false);
+        searchView.setIconifiedByDefault(false);
         return true;
     }
     @Override
